ba4790390165d7d257c4d3466e54dc2a43b1bfa4
aladlla <adalla@al-adalla.com>
2024-09-07 22:16:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     particlesJS('background-animation', {
         particles: {
-            number: { value: 80, density: { enable: true, value_area: 800 } },
-            color: { value: "#ffffff" },
+            number: { value: 150, density: { enable: true, value_area: 1000 } },
+            color: { value: ["#ff007a", "#007aff", "#ff8e71", "#ffcc70"] },
             shape: {
                 type: "circle",
                 stroke: { width: 0, color: "#000000" },
                 polygon: { nb_sides: 5 }
             },
             opacity: { value: 0.5, random: true },
-            size: { value: 3, random: true },
+            size: { value: 4, random: true },
             line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.2, width: 1 },
-            move: { enable: true, speed: 2, direction: "none", random: true }
+            move: { enable: true, speed: 3, direction: "none", random: true }
         },
         interactivity: {
             detect_on: "canvas",
